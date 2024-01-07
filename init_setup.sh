5a1e5d47ea5231636f33725be314e7dfae038990
Tzuting Huang <109066559+tzutingspace@users.noreply.github.com>
2024-01-07 15:32:21 +0800

    remove venv method

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
 echo "[ `date` ]": "START"
-echo "[ `date` ]": "Creating virtual env" 
-python -m venv venv/
-echo "[ `date` ]": "activate venv"
-source venv/bin/activate
+# echo "[ `date` ]": "Creating virtual env" 
+# python -m venv venv/
+# echo "[ `date` ]": "activate venv"
+# source venv/bin/activate
 echo "[ `date` ]": "installing the requirements" 
 pip install -r requirements.txt
 echo "[ `date` ]": "creating folders and files" 
